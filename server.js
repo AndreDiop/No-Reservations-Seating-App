2f9b927682df8264e8260e3ef53d4c9bf48d883c
Andre Diop <itsjustandre@gmail.com>
2021-01-14 14:03:43 -0500

    added server info
    - changed dir in server
    - changed dir in home html

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ var tables = [
   },
 ];
 // CREATE AN ARRAY FOR WAITLIST
+var waitList = [
+  {
+    name: "Yoda",
+    phone: "Jedi Master",
+    email: 900,
+    id: 2000,
+  },
+];
 // ARRAYS OF OBJECTS. OBJECTS SHOULD HAVE
 // NAME
 // PHONE
@@ -32,45 +40,42 @@ var tables = [
 
 // ROUTE FOR HOME
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
 // ROUTE FOR TABLES
 app.get("/tables", function (req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+  res.sendFile(path.join(__dirname, "views", "tables.html"));
 });
 
 // // ROUTE FOR RESERVE
 app.get("/reserve", function (req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+  res.sendFile(path.join(__dirname, "reserve", "reserve.html"));
 });
 // API ROUTES
 
 // GET ALL TABLE DATA
-app.get("/api/tables", function(req, res) {
-    return res.json(tables);
-  });
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
 // CREATE A TABLE/RESERVATION
-app.post("/api/tables", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newTables = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newTables
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newTables);
-  
-    characters.push(newTables);
-  
-    res.json(newTables);
-  });
-  
+app.post("/api/tables", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newTables = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newTables
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newTables);
+
+  characters.push(newTables);
+
+  res.json(newTables);
+});
+
 // LISTEN ON THE PORT
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-
-
-
