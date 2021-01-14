21b66e281878ae72d99be46d91baee0547ddb0d6
Andre Diop <itsjustandre@gmail.com>
2021-01-14 13:19:30 -0500

    updated with correct html files
    - added home, reserve, and tables files

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+// require express
 
+const { runInContext } = require("vm")
+
+create a port for the app to runInContext
+also listne to the enviomment variannb;es sp we can deplot to heorki
+
+add middleware 
+copy paste from star wars app 
+
+create arra yof tables
+create array for waitlist
+
+array of objects which Have
+nam
